15f49b3d4df133f7c8de0b4389b6d3b63e74231f
Preetha-21 <preethakaliappan289@gmail.com>
2026-05-14 22:12:24 +0530

    Add files via upload

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+function Footer() {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>
+            COMPANY
+          </h2><ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+
+            <li>Priveacy policy</li>
+          </ul>
+
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-211-123-12345</li>
+            <li>contact@tomato.com</li>
+          </ul>
+
+        </div>
+      </div>
+      <hr />
+      <p className='footer-copyright'>Copyright 2024 @ Tomato.com - All Right Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Savor every bite! Explore our diverse menu crafted from the finest ingredients — from sizzling starters to indulgent desserts, there’s something delicious for everyone.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
